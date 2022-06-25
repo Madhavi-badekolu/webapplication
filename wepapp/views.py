df3bb589d38afbaa1f250384a8d54437b829b1fe
madhavi-badekolu <madhavi@de-haze.com>
2022-06-25 14:00:53 +0530

    changes in views.py

--- wepapp/views.py
+++ wepapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def greetings(request):
-    return HttpResponse('Hi, Welcome to de-haze.com')
+    return HttpResponse('Hello,Welcome to de-haze.com organization')
