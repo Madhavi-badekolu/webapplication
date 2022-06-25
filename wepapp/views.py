c8879beecba1566f56989148765805877cecb6c8
madhavi-badekolu <madhavi@de-haze.com>
2022-06-25 18:27:02 +0530

    removed print statement

--- wepapp/views.py
+++ wepapp/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def greetings(request):
-    print('Entered into de-haze.com')
     return HttpResponse('Helllooo,Welcome to de-haze.com organization, Nice to have you here.')
