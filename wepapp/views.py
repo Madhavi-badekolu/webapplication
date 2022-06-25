ff3ae24eeedb88f3e24cbc61a0575a070dd79229
madhavi-badekolu <madhavi@de-haze.com>
2022-06-25 15:02:41 +0530

    added nict to have you here

--- wepapp/views.py
+++ wepapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def greetings(request):
-    return HttpResponse('Helllooo,Welcome to de-haze.com organization')
+    return HttpResponse('Helllooo,Welcome to de-haze.com organization, Nice to have you here.')
